abc2f85ac7c4011db4a61399cb24134a2e50fd8c
Qiageng Berke Jaisyurrohman <155612118+syberke@users.noreply.github.com>
2026-07-14 21:28:40 +0700

    fix: keep seeded admin credentials stable on repeated seeding

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -10,7 +10,7 @@ class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
+        User::firstOrCreate(
             ['email' => 'admin@hotel.com'],
             [
                 'name' => 'Super Admin',
