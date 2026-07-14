b06542a42fd481f9e4f3ae5dea070f9c7dde8907
Qiageng Berke Jaisyurrohman <155612118+syberke@users.noreply.github.com>
2026-07-14 21:28:53 +0700

    fix: keep seeded manager credentials stable on repeated seeding

--- database/seeders/ManagerUserSeeder.php
+++ database/seeders/ManagerUserSeeder.php
@@ -10,7 +10,7 @@ class ManagerUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::updateOrCreate(
+        User::firstOrCreate(
             ['email' => 'manager@hotel.com'],
             [
                 'name' => 'Hotel Manager',
