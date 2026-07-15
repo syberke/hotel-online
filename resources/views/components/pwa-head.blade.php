db7699494a53ce842e81cb1e11d39f550ae7c734
Qiageng Berke Jaisyurrohman <155612118+syberke@users.noreply.github.com>
2026-07-15 13:05:03 +0700

    feat: add shared PWA head metadata

---
+++ resources/views/components/pwa-head.blade.php
@@ -0,0 +1,7 @@
+<meta name="theme-color" content="#0a0a0a">
+<meta name="application-name" content="Oasis">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="apple-mobile-web-app-title" content="Oasis">
+<link rel="manifest" href="/manifest.json">
+<link rel="icon" href="/icons/oasis-pwa.svg" type="image/svg+xml">
