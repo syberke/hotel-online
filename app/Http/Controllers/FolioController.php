aaf14be2265c5d5de42799c1be76ceabac0fcc30
Qiageng Berke Jaisyurrohman <155612118+syberke@users.noreply.github.com>
2026-07-17 20:54:24 +0700

    Remove unused folio import

--- app/Http/Controllers/FolioController.php
+++ app/Http/Controllers/FolioController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
