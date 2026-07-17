fed047b37523cc4c36f4b7367cb958e1e5f93cb2
Qiageng Berke Jaisyurrohman <155612118+syberke@users.noreply.github.com>
2026-07-17 18:01:11 +0700

    Use modern PWA theme and install UI

--- resources/views/components/pwa-head.blade.php
+++ resources/views/components/pwa-head.blade.php
@@ -1,8 +1,9 @@
-<meta name="theme-color" content="#0a0a0a">
+<meta name="theme-color" content="#2563eb">
 <meta name="application-name" content="Oasis">
 <meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
 <meta name="apple-mobile-web-app-title" content="Oasis">
 <link rel="manifest" href="/manifest.json">
 <link rel="icon" href="/icons/oasis-pwa.svg" type="image/svg+xml">
+<link rel="stylesheet" href="/pwa-ui.css">
 <script src="/pwa.js" defer></script>
