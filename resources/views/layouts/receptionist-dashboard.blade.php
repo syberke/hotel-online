ed1f96a96b40f18778055157694c495b67003159
Qiageng Berke Jaisyurrohman <155612118+syberke@users.noreply.github.com>
2026-07-17 14:45:22 +0700

    Add PWA metadata to receptionist dashboard layout

--- resources/views/layouts/receptionist-dashboard.blade.php
+++ resources/views/layouts/receptionist-dashboard.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Oasis Hotel & Resort - Front Desk Portal</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <x-pwa-head />
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet">
